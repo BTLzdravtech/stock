1669aaa6c685d46eaa5729274f4912827b8c780d
alain <alainbloos@gmail.com>
2026-03-10 16:50:09 -0300

    [FIX] migration scripts: fix model for stock_voucher fields

--- stock_voucher/migrations/18.0.1.5.0/pre-migration.py
+++ stock_voucher/migrations/18.0.1.5.0/pre-migration.py
@@ -9,14 +9,14 @@ _logger = logging.getLogger(__name__)
 def migrate(env, version):
     cr = env.cr
 
-    _logger.info("START add vouchers to stock_move")
+    _logger.info("START add vouchers to stock_picking")
     openupgrade.add_columns(env, [
-        ("stock.move", "vouchers", "char"),
+        ("stock_picking", "vouchers", "char"),
     ])
-    _logger.info("END add vouchers to stock_move")
+    _logger.info("END add vouchers to stock_picking")
 
-    _logger.info("START add declared_value to stock_move")
+    _logger.info("START add declared_value to stock_picking")
     openupgrade.add_columns(env, [
-        ("stock.move", "declared_value", "float"),
+        ("stock_picking", "declared_value", "float"),
     ])
-    _logger.info("END add declared_value to stock_move")
+    _logger.info("END add declared_value to stock_picking")
